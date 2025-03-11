81ac5f35ab0e9017f5377b6f2db45dac0c3e4fa5
Balint Molnar <balint.molnar.dev@gmail.com>
2025-03-11 18:25:35 +0100

    fix: auto reload (~uvicorn/gunicorn)

--- src/run.py
+++ src/run.py
@@ -1,18 +1,19 @@
 import os
+from pathlib import Path
 
 from connexion import FlaskApp
 from dotenv import load_dotenv
 
+load_dotenv()
+
 app = FlaskApp(__name__)
 
-load_dotenv()
+app.add_api(specification="../openapi.yaml")
+app.app.config["WEATHER_API_KEY"] = os.getenv("WEATHER_API_KEY")
 
 
 def main() -> None:
-    app.add_api(specification="../openapi.yaml")
-    app.app.config["WEATHER_API_KEY"] = os.getenv("WEATHER_API_KEY")
-
-    app.run()
+    app.run(f"{Path(__file__).stem}:app")
 
 
 if __name__ == "__main__":
