44117c4c6763e962a625f193d41f475eeb37ec4c
Balint Molnar <balint.molnar.dev@gmail.com>
2025-03-11 17:21:10 +0100

    fix: flake8 errors

--- src/run.py
+++ src/run.py
@@ -1,8 +1,7 @@
 import os
+
 from connexion import FlaskApp
 from dotenv import load_dotenv
-from pathlib import Path
-
 
 app = FlaskApp(__name__)
 
@@ -11,7 +10,6 @@ load_dotenv()
 
 def main() -> None:
     app.add_api(specification="../openapi.yaml")
-    # app.run(f"{Path(__file__).stem}:app")
     app.app.config["WEATHER_API_KEY"] = os.getenv("WEATHER_API_KEY")
 
     app.run()
